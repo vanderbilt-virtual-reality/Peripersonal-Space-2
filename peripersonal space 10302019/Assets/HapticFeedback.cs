cbe2f7306fb179f6b685cf132d2c7644a1155a5c
huk1 <kevin.hu@vanderbilt.edu>
2019-10-30 20:06:52 -0500

    Updated project
    Added the current state of the project with moving avatars within a classroom type environment with haptic feedback.

---
+++ peripersonal space 10302019/Assets/HapticFeedback.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HapticFeedback : MonoBehaviour
+{ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(GameObject.FindGameObjectWithTag("OVRPlayer").transform.position, GameObject.FindGameObjectWithTag("Avatar").transform.position) == 2){
+            print("Hi!!!");
+            OVRInput.SetControllerVibration(1, 1, OVRInput.Controller.LTouch);
+            OVRInput.SetControllerVibration(1, 1, OVRInput.Controller.RTouch);
+        }
+    }
+}
